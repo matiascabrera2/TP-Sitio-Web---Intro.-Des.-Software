7db339bf0d7e1d7f35dae077f8ff4a2f32a6f134
ArielRepo <cruzgastonariel@yahoo.com>
2025-01-24 10:44:23 -0300

    add basic CRUD structure for index.html(endpoints only)

--- todo_backend/src/index.js
+++ todo_backend/src/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
-
+import axios from 'axios';      // Manejo de peticiones http, mas facil
 
 
 const app = express()
@@ -10,7 +10,46 @@ const prisma = new PrismaClient();
 
 app.use(express.json());
 
+// ------- HOME ---------
+app.get('/', async (req, res) => {
+    try {
+        const response = await axios.get('http://frontend:80/index.html');
+        res.send(response.data);
+        } catch (error) {
+        console.error('Error al obtener la página estática:', error);
+        res.status(500).send('No se pudo obtener la página estática');
+        }
+});
 
+//  ------- ENDPOINTS --------
+app.get('/api/libros',  async (req, res) => {
+    // Busco listar todo
+    const authors = await prisma.author.findMany()
+    res.json(authors)
+})
+
+app.get('/api/libros/:id', async (req, res) => {
+    // Busco listar solo un libro
+})
+
+app.post('/api/books', async (req, res) =>{
+    // Agrega nuevo libro/autore
+
+})
+
+app.put('/api/libros/:id', async (req, res) => {
+    // actualiza reescribiendo todos los datos de un libro
+
+})
+
+app.put('/api/libros/:id', async (req, res) => {
+    // actualiza parcialmente, reescribiendo los datos ingresados de un libro
+
+})
+
+app.delete('/api/libros/:id', async (req, res) => {
+    // Borrar un libro en especifico
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
